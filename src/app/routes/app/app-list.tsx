4232613698aae62550bc0e39c5d5d6b7397c4f8d
Shahanwaz <mohdshahanwaz55@gmail.com>
2024-12-17 00:17:11 +0530

    fix: remove cb after logout in app-list

--- src/app/routes/app/app-list.tsx
+++ src/app/routes/app/app-list.tsx
@@ -2,18 +2,13 @@ import { AppListLayout } from '@/components/layouts/app-list-layout'
 import { Button } from '@/components/ui/button'
 import { ProtectedRoute } from '@/features/auth/components/protected-route'
 import { logout } from '@/hooks/use-auth'
-import { useNavigate } from 'react-router-dom'
 
 export const AppListRoute = () => {
-  const navigate = useNavigate()
-
-  const handleLogout = () => logout(() => navigate('/auth/login'))
-
   return (
     <ProtectedRoute>
       <AppListLayout>
         <div>ALL APPS</div>
-        <Button onClick={handleLogout}>Logout</Button>
+        <Button onClick={logout}>Logout</Button>
       </AppListLayout>
     </ProtectedRoute>
   )
